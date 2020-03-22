10976e7e8a35f8f2958bab71bff143699d02a98a
pedropech2001 <pedroluispech3@gmail.com>
2020-03-22 17:00:26 -0600

    change

--- exercise10.c
+++ exercise10.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
+int main(int argc, char *states[])
 {
  int i = 0;
  // go through each string in argv
  // why am I skipping argv[0]?
  for(i = 1; i < argc; i++) {
- printf("arg %d: %s\n", i, argv[i]);
+// in this part i change the code to put the variables in the armunts line
+ printf("arg %d: %s\n", i, states[i]);
  }
  // let's make our own array of strings
- char *states[] = {"California", "Oregon","Washington", "Texas"};
+ //char *states[] = {"California", "Oregon","Washington", "Texas"};
  int num_states = 4;
-
+/* the for have 3 parts (all separate by commas) the firts where you put the variable and its value,
+the second where you put a condition and the last where you put what happens if the condition its ture*/
  for(i = 0; i < num_states; i++) {
  printf("state %d: %s\n", i, states[i]);
  }
--- exercise9.c
+++ exercise9.c
@@ -4,33 +4,31 @@ int main(int argc, char *argv[])
 {
  int numbers[4] = {0};
  char name[4] = {'a'};
- // first, print them out raw
+
  printf("numbers: %d %d %d %d\n",numbers[0], numbers[1],numbers[2], numbers[3]);
 
  printf("name each: %c %c %c %c\n",name[0], name[1],name[2], name[3]);
 
  printf("name: %s\n", name);
 
- // setup the numbers
-/*if you put chars in an int you will recibe their values in the ascii code*/
+/*if you put chars in an int you will receive their values in the ascii code*/
  numbers[0] = 'a';
  numbers[1] = 'B';
  numbers[2] = 'A';
  numbers[3] = 4;
 printf("numbers: %d %d %d %d\n",numbers[0], numbers[1],numbers[2], numbers[3]);
+/*when you put a number like a char you will receive its value in the ascii*/
 
-
- // setup the name
 
  name[0] = 66;
  name[1] = 50;
  name[2] = 65;
-name[3] = 37;
- // then print them out initialized
+ name[3] = 37;
+
 
  printf("numbers: %d %d %d %d\n",numbers[0], numbers[1],numbers[2], numbers[3]);
 
-printf("name each: %c %c %c %c\n",name[0], name[1],name[2], name[3]);
+ printf("name each: %c %c %c %c\n",name[0], name[1],name[2], name[3]);
 
  // print the name like a string
 
