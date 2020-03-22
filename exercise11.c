60c972c141f061eb7038d9e82971a6ef8f526cb6
pedropech2001 <pedroluispech3@gmail.com>
2020-03-22 17:11:46 -0600

    exe11 commit

--- exercise11.c
+++ exercise11.c
@@ -2,19 +2,20 @@
 int main(int argc, char *argv[])
 {
  // go through each string in argv
- int i = 0;
- while(i < argc) {
+ int i = argc-1;
+ while(i  >=  0) {
+/* I make some changes to print my argv in reverse order changing the i valuve and the condition*/
  printf("arg %d: %s\n", i, argv[i]);
- i++;
+ i--;
  }
  // let's make our own array of strings
  char *states[] = { "California", "Oregon","Washington", "Texas"
  };
  int num_states = 4;
- i = 0; // watch for this
- while(i < num_states) {
+ i = num_states-1; // watch for this
+ while(i >= 0) {
  printf("state %d: %s\n", i, states[i]);
- i++;
+ i--;
  }
  return 0;
 }
